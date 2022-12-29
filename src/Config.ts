cb127bf859d2c79435a3fa98cc56fa52b3abd3bd
httpsnet <87951297+httpsnet@users.noreply.github.com>
2022-12-29 10:18:20 +0900

    testing

--- src/Config.ts
+++ src/Config.ts
@@ -9,7 +9,7 @@ export const version = '2.10.0: December 26 v1: Code of Codes';
  * PSEUDO DO NOT CHANGE THIS LINE
  */
 // eslint-disable-next-line
-export const testing: boolean = false;
+export const testing: boolean = true;
 export const lastUpdated = new Date('##LAST_UPDATED##');
 /**
  * CHANGE THIS ONE INSTEAD
