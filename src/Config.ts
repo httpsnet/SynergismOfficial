a8dc7f6c9bf5b2f2a707169f53b9856102cc34c5
httpsnet <87951297+httpsnet@users.noreply.github.com>
2025-01-05 18:34:34 +0900

    give testing

--- src/Config.ts
+++ src/Config.ts
@@ -8,9 +8,9 @@ export const version = '3.0.0 pt 6: More Exalts Mod'
  * PSEUDO DO NOT CHANGE THIS LINE
  * PSEUDO DO NOT CHANGE THIS LINE
  */
-export const testing: boolean = true
+export const testing: boolean = !false
 export const lastUpdated = new Date('##LAST_UPDATED##')
 /**
  * CHANGE THIS ONE INSTEAD
  */
-export const prod: boolean = false
+export const prod: boolean = !false
